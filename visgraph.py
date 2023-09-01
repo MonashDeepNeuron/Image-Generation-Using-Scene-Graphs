d6b8344dadf0aa379828131cc2493fb30caf2f56
Nyan Kyaw <nkya0002@student.monash.edu>
2023-09-01 19:32:11 +1000

    Implemented the SceneGraphConstructor class and its construct_scene_graph method, which returns a PyTorch Geometric Data object encoding the scene graph information (object nodes, supernode & predicate nodes). TODO: Modify Kruskal's algorithm such that every vertex in the minimum spanning tree has a degree of at most 1, to decentralize the MST

---
+++ visgraph.py
@@ -0,0 +1,39 @@
+# import torch
+# import torch_geometric
+# import networkx as nx
+# import matplotlib as plt
+# g = torch.load('please_god.pt')
+# g = torch_geometric.utils.to_networkx(g, to_undirected=False)
+# nx.draw_networkx(g)
+# plt.show()
+import torch
+import torch_geometric
+import networkx as nx
+import matplotlib.pyplot as plt  # Note the change here
+
+g = torch.load('please_god.pt')
+labeldict = {}
+i = 0
+for node in g.x:
+    labeldict[i] = node[0]
+    i += 1
+    
+g = torch_geometric.utils.to_networkx(g, to_undirected=False)
+
+# Check if the graph has nodes and edges
+if not g.nodes():
+    print("The graph has no nodes!")
+if not g.edges():
+    print("The graph has no edges!")
+
+# # Draw the graph
+# #plt.ioff()  # Turn off interactive mode
+# nx.draw_networkx(g)
+# plt.show()  # This will show the plot
+
+# Use a different layout
+pos = nx.spring_layout(g)
+
+plt.figure(figsize=(12,12))  # Increase the figure size for better visibility
+nx.draw(g, pos, labels=labeldict, node_size=50, edge_color="gray", with_labels=True)
+plt.show()
