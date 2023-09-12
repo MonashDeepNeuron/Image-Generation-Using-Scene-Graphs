78c1c8d42cb2efce7c46ec7f1b7d97acb336e027
AbbasQuettawala <131154280+AbbasQuettawala@users.noreply.github.com>
2023-09-12 19:51:27 +1000

    Update Preparing_Visual_Genome_Dataset.py
    Nearly done but need to finish bbox

--- src/Preparing_Visual_Genome_Dataset.py
+++ src/Preparing_Visual_Genome_Dataset.py
@@ -29,13 +29,11 @@ from torch_geometric.data import Dataset, download_url
 import networkx as nx
 from networkx.algorithms import tree
 
-from .utils import imagenet_preprocess, Resize
-
 
 
 class VisualGenomeDataset(Dataset):
   """ 
-  A PyTorch Dataset for loading Visual Genome dataset image and converting
+  A PyTorch Dataset for loading Visual Genome dataset image and
   getting information from them.
   
 
@@ -44,58 +42,139 @@ class VisualGenomeDataset(Dataset):
   - instances_json: Path to a JSON file giving COCO annotations (str-path)
   - image_size: Size (H, W) at which to load images. 
   - mask_size: Size M for object segmentation masks; default 16. (int)
+  - object_dir
   """
 
-  def __init__(self, image_id, image_dir, object_name, object_id, image_size=(256, 256)):
-    super(VisualGenomeDataset, self).__init__()
+  def __init__(self, image_id, image_dir, image_data,object_dir,object_name, object_id, image_size=(256, 256),min_object_size=0.02):
+    super(VisualGenomeDataset, self).__init__() 
 
+    # Listing 
     self.image_dir = image_dir
     self.image_size = image_size
     self.image_id = image_id
     self.object_name = object_name
+    self.object_id = object_id
+    self.image_data = image_data
+    self.object_dir = object_dir 
     
 
-  #Open file 
-    with open(image_data, 'r') as f: # r => the file will be opened in read mode but not for modifying.
-      image_data = json.load(f)
-    
+  #Open image_data json file 
+    with open(image_dir, 'r') as f: # r => the file will be opened in read mode but not for modifying.
+      image_data = json.load(f) 
 
-    with open(object_json, 'r') as f: # r => the file will be opened in read mode but not for modifying.
-      object_data = json.load(f)
-
-
-     # Storage
-    self.img_id_store = []
-    self.img_id_to_url = {}
-    self.obj_id_to_size = {}
-    
+     #Open object_data json file 
+    with open(object_dir, 'r') as f: # r => the file will be opened in read mode but not for modifying.
+      object_data = json.load(f) 
 
 
-    for item in object_data['images']:
-      object_id =  object_data['image_id']
-      file_url= image_data['url']
-      width = object_data['w']
-      height = object_data['h']
-      self.img_id_store.append(object_id)
-      self.img_id_to_url[object_id] = file_url
-      self.obj_id_to_size[object_id] = (width, height)
+    # Image_Data Storage
+    self.img_id_store = [] 
+    self.img_id_to_url= {} 
+    self.img_id_to_size = {}
+    self.img_id_to_objects = {}
 
+      
+    '''
+    Process each image
+    '''
+    for item in image_data:# IF AN ERROR OCCURS (CANNOT INDEX), for item in image_data['images']:
+          image_id =  item['image_id']
+          file_url= item['url']
+          width = item['width']
+          height = item['height']
+          self.img_id_store.append(image_id)
+          self.img_id_to_url[image_id] = file_url
+          self.img_id_to_size[image_id] = (width, height)  #Ok upto here
+
+          
+    # Object_Data storage
     # dictionary storing index and name/categories of the image
     self.dict_vocab = {
-       'object_name_to_idx': {},
-       'pred_name_to_idx': {},
-  
+       'object_name_to_idx': {}, # 'image_id' to index in img_id_store list 
+       'pred_name_to_idx': {}, # pred_name to index in img_id_store list 
     }
+
+    object_idx_to_name = {} ### maps every index value to an object name ###
+    all_instance_categories = [] ### List of all class names in dataset ###
+    obj_id_to_img_id = {} ### maps every object ID to its image ID
 
     obj_idx_to_name ={}
     # extract the categories/names of the object from object_data
-    all_object_categories= []
-    for category_data in object_data:
-       category_id = category_data['object_id']
-       category_name =  category_data['names']
-       all_object_categories.append[category_name]
-       obj_idx_to_name[category_id] = category_name
-       self.dict_vocab['object_name_to_idx'][category_name] = category_id
+    
+    
+    '''
+    Process each object
+    '''
+
+    for index in range(0,len(object_data)): # no need len(object_data) - 1 as range(0,x) is not x inclusive
+       # object_data[index]: # IF AN ERROR OCCURS (CANNOT INDEX), for item in image_data['categories']:
+
+       # Retrieving the 'Object ID' (e.g. 10000223, 231, the unique numerical identifier for the object)
+       object_id = object_data[index]['object_id'] 
+
+       # Retrieving the 'Category Name' of the object (e.g. 'Apple', 'Car')
+       category_id = object_data[index]['synsets'][0]
+       
+       #object_data[index]['synsets'][0]
+
+      # Mapping the 'Object ID' to its corresponding 'Category Name' 
+       obj_idx_to_name[object_id] = category_id
+
+      # Mapping the 'Object ID' to its corresponding image 'Image ID'
+       obj_id_to_img_id[object_id] = object_data[index]['image_id']
+
+      # # Mapping the 'Object ID' to its corresponding image 'Image ID'
+      #  self.dict_vocab['object_name_to_idx'][category_name] = category_id
+       
+       # Building the all_instance_categories list which holds every category this dataset contains (e.g. 'Apple', 'Car')
+       if category_id not in all_instance_categories:
+          all_instance_categories.append(category_id)
+          
+
+    # Add object data from instances
+    self.img_id_to_objects = defaultdict(list) ### This maps an image to its objects, previously we just mapped class id to objects ###
+
+    '''
+    Calculating the bounding boxes for each object
+    '''
+    for index in range(0,len(object_data)):
+    #for object_data in image_data:
+      image_id = object_data[index]['image_id']
+
+      # width and height of the object
+      w = object_data[index]['w']
+      h = object_data[index]['h']
+      
+      # object_data[index]['synsets'][0] = category_id in docodatase
+
+
+     # width and height of the image
+      width,height = self.img_id_to_size[image_id]
+      box_area = (w * h) / (width * height) ### used to calculate min box_area
+      box_ok = box_area > min_object_size
+      object_name = object_idx_to_name[object_data[index]['synsets'][0]] ### Again using dictionaries helpful for referencing
+      category_ok = object_name in category_whitelist 
+      other_ok = object_name != 'other' or include_other ### Don't include object if its annotated as "other"
+      if box_ok and category_ok and other_ok:
+        self.image_id_to_objects[image_id].append(object_data[index]) ### We can use this object ###
+
+
+
+
+
+
+
+       
+  '''
+  if stuff_data:
+      for category_data in stuff_data['categories']:
+          category_name = category_data['name']
+          category_id = category_data['id']
+          all_stuff_categories.append(category_name) ### Stores reference of the current "stuff" name ###
+          object_idx_to_name[category_id] = category_name ### Treats "stuff" and object instances as the same, useful for referencing ###
+          self.vocab['object_name_to_idx'][category_name] = category_id
+  '''
+
 
     '''
    
@@ -109,79 +188,25 @@ class VisualGenomeDataset(Dataset):
           self.vocab['object_name_to_idx'][category_name] = category_id
      '''
        
-       
-
-
-    self.transform = T.Compose([T.Resize(image_size), T.ToTensor()])
-    border_info ={y0_bottom_left:0, y1_top_left:0, x0_bottom_left:0, y1_top_right:0, x_width:0 , y_height:0}
-    
-    # pass all info into dictionary border_info
-    
-    border_info[x0_bottom_left]= x0
-    border_info[y0_bottom_left]=  y0
-    border_info[x1_top_right]= x1
-    border_info[y1_top_right]= y1
-    border_info[x_width]= img_dimension[0]
-    border_info[y_height] = img_dimension[1]
-
-    # [x_width, y_height]
-    self.img_dimension = img_dimension
-    img_dimension = []
-    #img_dimension[0]= self
-
-  # Psuedo
-    bt_left_x0y0 = [x0,y0] 
-    tp_right_x1y1 = [x1,y1]
-
-    # check 
-    resize = (0.5,0.5) # Between 0 and 1 (worry about later)
-    org_size = [bt_left_x0y0, tp_right_x1y1]
-    org_size[0] = x0_bottom_left
-
- 
-
-
-
-
-
-  # Trying to extract image dimensions from an image (leave for later)
-  def get_image_size(self,index):
-    index = 0
-    img_path = os.path.join(self.image_dir, self.image_paths[index])
+      
   
-  def __len__(self):
-        return len(self.image_paths)
-
-   
-  def __getitem__(self, index):
-    img_path = os.path.join(self.image_dir, self.image_paths[index])
-        
-      with open(img_path, 'rb') as f:
-          with PIL.Image.open(f) as image:
-              width, height = image.size
-              image = self.transform(image.convert('RGB'))
-        
-      # Process the object information here
-      object_info = self.object_info[index]
-
-      return {
-            'image': image,
-            'object_info': object_info,
-            'image_size': (width, height)
-        }
-  
+    self.sceneGraphConstructor = mst_algo.SceneGraphConstructor(vocab=self.vocab, boxes=boxes)
+    scene_graph = self.sceneGraphConstructor.construct_scene_graph(objs, boxes, masks)
+    g = torch_geometric.utils.to_networkx(scene_graph, to_undirected=False)
+    nx.draw(g)
+    torch.save(scene_graph, "please_god.pt")
 
 
 # Main Function:
 
 
 if __name__ == "__main__":
-    image_directory = "/Users/abbas/Desktop/Engineering/Coding/Deep-Neuron/Datasets/Image-Generation-Dataset-(Visual-Genome)/Visual_Genome_Part2_Images"
+    image_dir = "/Users/abbas/Desktop/Engineering/Coding/Deep-Neuron/Datasets/Image-Generation-Dataset-(Visual-Genome)/Visual_Genome_Part2_Images"
     image_paths = [...]  # List of image file paths
     object_info = [...]  # List of dictionaries containing object info
 
     image_size = (256, 256)  # Example image size
-    dataset = VisualGenomeDataset(image_directory, image_paths, object_info, image_size)
+    dataset = VisualGenomeDataset(image_dir, image_paths, object_info, image_size)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
     # Now you can use the dataloader to iterate over your dataset in batches
